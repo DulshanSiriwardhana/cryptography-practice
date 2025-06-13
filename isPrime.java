07b1417ecc558fbfc411b0eec747ddf3ea3e9464
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-06-13 11:01:38 +0530

    isPrime

--- isPrime.java
+++ isPrime.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class isPrime {
     public static void main(String[] args) {
         while (true) {
+            //To get the input
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter an integer: ");
             int n = scanner.nextInt();
 
             boolean isPrime = true;
 
+
+            //This if conditoin if set to avoid negative value and, so any integer less than 2 is not a prime!
             if (n <= 1) {
                 isPrime = false;
             } else {
+                //We can set the for loop starting from 2 - sqrt(n), 
+                //because any there are two pair of divisors always if one is less than square root of n other one is greater than that, so checking one is enough
                 for (int i = 2; i <= Math.sqrt(n); i++) {
                     if (n % i == 0) {
                         isPrime = false;
