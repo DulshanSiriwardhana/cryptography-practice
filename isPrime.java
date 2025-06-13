02f0b66e347e962bfcce7e60e608340c5b415cbc
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-06-13 10:57:30 +0530

    init

---
+++ isPrime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class isPrime {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter an integer: ");
+            int n = scanner.nextInt();
+
+            boolean isPrime = true;
+
+            if (n <= 1) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(n); i++) {
+                    if (n % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(n + " is a prime number.");
+            } else {
+                System.out.println(n + " is not a prime number.");
+            }
+        }
+    }
+}
